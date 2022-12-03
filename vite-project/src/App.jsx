a147e7a992946c202b3ce0930916ce696868b832
Francisco Navarro <fran27_navarro@hotmail.com>
2022-12-02 23:02:01 -0300

    mobile menu not completed

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
          className='nav__toggle'
          >
           {/* menu icon-------------------------------  */}
-          <svg className='menu' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <svg className='menu' xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
           </svg>
           {/* ----------------------------------------- */}

63bafd062b273f52feecb434c51aa782ea37cb0c
Francisco Navarro <fran27_navarro@hotmail.com>
2022-12-02 22:43:31 -0300

    mobile menu not completed

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Logo_fran from './assets/fran-dev-logo-claro.png';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +19,28 @@ function App() {
         <label
          htmlFor='nav__checkbox'
          className='nav__toggle'
-         ></label>
+         >
+          {/* menu icon-------------------------------  */}
+          <svg className='menu' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+          {/* ----------------------------------------- */}
+
+          {/* menu icon close-------------------------- */}
+          <svg className='close' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+          </svg>
+          {/* ----------------------------------------- */}
+        </label>
+        <ul className='nav__menu'>
+          <li><a href="#"><img className='logo_fran' src={Logo_fran} alt="logo" /></a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About me</a></li>
+          <li><a href="#">Skills</a></li>
+          <li><a href="#">My projects</a></li>
+          <li><a href="#">Contact me</a></li>
+          
+        </ul>
       </nav>
     </div>
 

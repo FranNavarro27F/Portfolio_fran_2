11e330c3d1fd959b49e0d4f59bd68122d66ce892
Francisco Navarro <fran27_navarro@hotmail.com>
2022-12-13 16:08:26 -0300

    dark and light modes work

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,32 +1,39 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+// import reactLogo from './assets/react.svg';
 import './App.css'
-import menu from "./assets/menu.svg";
-import fran_logo from "./assets/fran-dev-logo-claro.png";
+import fran_logo_claro from "./assets/fran-dev-logo-claro.png";
+import fran_logo_negro from "./assets/fran-dev-logo-negro.png";
 import sun from "./assets/sun.svg";
 import moon from "./assets/moon.svg";
 import React, { useRef } from 'react';
 
 function App() {
 
-  // const buttonRef= useRef(null);
-  // // console.log(buttonRef)
+  let [mode, setMode]= useState("dark");
+  let logos= mode === "dark" ? fran_logo_claro : fran_logo_negro;
 
-  // const switch_mode= ()=> {
-  //   buttonRef.current.className= 'rojo';
-  // };
+  const handleMode= ()=> {
+    mode === "dark" ? setMode("light") : setMode("dark");
+  }
+  
 
   const button_Dark_Light_ref= useRef(null);
   const dark_light_active_ref= useRef(null);
  
 
   const switch_mode= ()=> {
-    if(button_Dark_Light_ref.current.id === ""){
-        button_Dark_Light_ref.current.id = "light";
-    }else if(button_Dark_Light_ref.current.id === "light"){
-      button_Dark_Light_ref.current.id= "dark";
-    }else if(button_Dark_Light_ref.current.id === "dark"){
-      button_Dark_Light_ref.current.id= "light";
+    if(button_Dark_Light_ref.current.className === ""){
+      button_Dark_Light_ref.current.className = "light"
+    }else if(button_Dark_Light_ref.current.className === "light"){
+      button_Dark_Light_ref.current.className= "dark"
+    }else if(button_Dark_Light_ref.current.className === "dark"){
+      button_Dark_Light_ref.current.className= "light"
+    }
+
+    if(mode === "dark"){
+       setMode("light")
+    }else if(mode === "light"){
+      setMode("dark")
     }
 
     if(dark_light_active_ref.current.className === "switch"){
@@ -37,23 +44,25 @@ function App() {
       dark_light_active_ref.current.className= "switch";
       return;
     }
-    
 
-  }
+
+
+  };
 
 
   return (
-   <div className='gridContainter' ref={button_Dark_Light_ref}>
+  <div ref={button_Dark_Light_ref}>
+   <div className='gridContainter'>
 
     <nav className='nav'>
       <div className='nav__container'>
         {/* <h1 className='nav__logo'>Fran</h1> */}
         <div className='logo'>
-          <img className='nav__logo' src={fran_logo} alt="logo" />
+          <img className='nav__logo' src={logos} alt="logo" />
           <span>Francisco Navarro</span>
         </div>
         <label className='nav__label' htmlFor="menu" >
-          <img className='nav__img' src={menu} alt="no hay imagen" />
+          <img className='nav__img' src={ mode === "dark" ? "/menu.svg" : "/menu_light.svg"} alt="img_menu" />
         </label>
         <input className='nav__input' type='checkbox' id='menu' />
 
@@ -62,8 +71,8 @@ function App() {
           <div className='language_and_light-dark'>
             <div className='language'>
               <button className='language-switch'>
-                <div><span><h3>English</h3></span></div>
-                <div><span><h3>Spanish</h3></span></div>
+                <div><span><h3>Eng</h3></span></div>
+                <div><span><h3>Spa</h3></span></div>
               </button>
             </div>
             <div className='light-dark'>
@@ -71,6 +80,7 @@ function App() {
                 id='switch'
                 className='switch'
                 onClick={switch_mode}
+                
                 ref={dark_light_active_ref}
                 >
                 <span><img src={sun} alt="sun" /></span>
@@ -115,7 +125,9 @@ function App() {
       <h1>Footer</h1>
     </div>
 
+
    </div>
+  </div>
   )
 }
 
